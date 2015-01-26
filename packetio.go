d3780aa01245df54d707563de25d5d0253520930
Taru Karttunen <taruti@taruti.net>
2015-01-27 00:12:46 +0200

    Package comment

--- packetio.go
+++ packetio.go
@@ -1,3 +1,10 @@
+/*Package packetio provides length delimeted typed serialization compatible with protobuf.
+
+packetio provides an easy way to delimit messages with length and a type. This makes
+serializing e.g. protobuf (gogoprotobuf) messages much easier. No dependency on
+protobuf however.
+
+*/
 package packetio
 
 import (
